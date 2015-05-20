44ece0c9f04b766bec2ebfa2b9ede60b1308bc89
Ubuntu 14 <daoyun.zeng@gmail.com>
2015-05-19 17:32:17 -0700

    Add an additional test file to chekc if the libary works

--- read.js
+++ read.js
@@ -4,3 +4,5 @@ gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
 });
 gpio.setup(17, gpio.DIR_IN);
+gpio.setup(18, gpio.DIR_IN);
+gpio.setup(23, gpio.DIR_IN);

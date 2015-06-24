ee07c857fa30121c99ca7f0c141ec129b6d254a7
wirtanen <u_key7@hotmail.com>
2015-06-24 16:10:44 -0700

    Update LEDtest.js
    Since this is simply a test to see if the for loop works with the buttons watch command it is unnecessary and inefficient to test all 9. So we are just testing 6 of the buttons and leds

--- LEDtest.js
+++ LEDtest.js
@@ -1,6 +1,6 @@
 
 var Gpio = require("onoff").Gpio,
-	//DD1
+
 	button1 = new Gpio(2, 'in', 'both'),
 	button2 = new Gpio(3, 'in', 'both'),
 	button3 = new Gpio(4, 'in', 'both'),
@@ -8,8 +8,7 @@ var Gpio = require("onoff").Gpio,
 	led1 = new Gpio(14, 'out'),
 	led2 = new Gpio(15, 'out'),
 	led3 = new Gpio(18, 'out'),
-
-	//DD2
+	
 	button4 = new Gpio(17, 'in', 'both'),
 	button5 = new Gpio(27, 'in', 'both'),
 	button6 = new Gpio(22, 'in', 'both'),
@@ -18,19 +17,10 @@ var Gpio = require("onoff").Gpio,
 	led5 = new Gpio(24, 'out'),
 	led6 = new Gpio(10, 'out'),
 
-	//DD3
-	button7 = new Gpio(11, 'in', 'both'),
-	button8 = new Gpio(8, 'in', 'both'),
-	button9 = new Gpio(7, 'in', 'both'),
-
-	led7 = new Gpio(16, 'out'),
-	led8 = new Gpio(20, 'out'),
-	led9 = new Gpio(21, 'out');
-
 var buttons = [button1, button2, button3, button4,button5,button6,button7,button8,button9];
 var leds = [led1, led2, led3, led4, led5,led6,led7,led8,led9];
 
-for(var i =0; i<9;i++)
+for(var i =0; i<6;i++)
 {
  button[i].watch(function(err,value){
  	led[i].writeSync(value);
